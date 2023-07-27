d78ee9dc5d431828067a8ace01284415b385b2de
oskari83 <oskpelto@gmail.com>
2023-07-27 17:50:43 +0300

    members area progress

--- src/app/members/page.js
+++ src/app/members/page.js
@@ -1,54 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import './members.css'
+import MembersArea from '@/components/membersarea'
 
 export default function Page(){
 
 	return(
 		<>
-			<div className='members-navbar'>
-				<div className='members-navbar-inner'>
-					<div className='members-navbarlinks-outer-container'>
-						<div className='members-navbar-link'>Overview</div>
-						<div className='members-navbar-link'>Resources</div>
-						<div className='members-navbar-link'>CIBS Connect</div>
-					</div>
-				</div>
-			</div>
-			<div className='members-banner-container'>
-				<div className='members-outer'>
-					<div className='members-header-container'>
-						<p>MEMBERS AREA</p>
-					</div>
-					<div className='thinLine'></div>
-					<div className='members-minor-container'>
-						<p>
-							Here, you will find updates on workshops and events organised by CIBS, as well as applications/technical resources and videos from CIBS Connect.
-						</p>
-					</div>
-
-					<div className='members-area-header-container'>
-						Application Guides & Resources
-					</div>
-					<div className='members-area-minor-text-container'>
-						We are working on a series of application guides to ensure you the best success during application season. These will include insights into the different divisions found at investment banks, tips for application processes, and much more. We will also update this page with resources from our workshops.
-					</div>
-
-					<div className='members-area-header-container'>
-						CIBS Connect
-					</div>
-					<div className='members-area-minor-text-container'>
-						CIBS Connect is an initiative created in order to provide more personal insights into the world of investment banking. We discuss pathways into the industry, skills and abilities needed for the role, and career progression. Tune in to our first two episodes with MDs from RBC Capital Markets and Morgan Stanley.
-					</div>
-
-					<div className='members-area-header-container'>
-						Social Media
-					</div>
-					<div className='members-area-minor-text-container'>
-						Follow CIBS on all social media to keep up-to-date with our events and competitions. Our LinkedIn page is also updated with links to the latest articles from our research group, which are a valuable resource in keeping in touch with the latest industry developments. Every week, four commercial awareness quiz questions are posted to our Instagram - a great way to see how much you've retained from this week's news! Finally, on Facebook, you can find all upcoming workshops and socials.
-					</div>
-				</div>
-			</div>
+			<MembersArea></MembersArea>
 		</>
 	)
 }
---
+++ src/components/membersarea.js
@@ -0,0 +1,303 @@
+'use client'
+
+import Image from 'next/image'
+import './membersarea.css'
+import { useState } from 'react'
+
+import springGuide_img from '../../resources/membersArea/springGuide.png'
+import workshop1_img from '../../resources/membersArea/workshop1.png'
+import workshop2_img from '../../resources/membersArea/workshop2.png'
+import workshop3_img from '../../resources/membersArea/workshop3.png'
+
+import connect_img from '../../resources/membersArea/connect.png'
+
+const MembersOverview = ({selectionFunc}) => {
+	return(
+		<>
+			<div className='members-banner-container'>
+				<div className='members-outer'>
+					<div className='members-header-container'>
+						<p>MEMBERS AREA</p>
+					</div>
+					<div className='thinLine'></div>
+					<div className='members-minor-container'>
+						<p>
+							Here, you will find updates on workshops and events organised by CIBS, as well as applications/technical resources and videos from CIBS Connect.
+						</p>
+					</div>
+
+					<div className='members-area-major-box-container'>
+						<div className='members-area-header-outer-container'>
+							<div className='members-area-header-container'>
+								Application Guides & Resources
+							</div>
+							<div className='members-area-button-outer-container'>
+								<div className='members-area-button-box' onClick={() => selectionFunc(1)}>
+									Access here
+								</div>
+							</div>
+						</div>
+				
+						<div className='members-area-minor-text-container'>
+							We are working on a series of application guides to ensure you the best success during application season. These will include insights into the different divisions found at investment banks, tips for application processes, and much more. We will also update this page with resources from our workshops.
+						</div>
+					</div>
+					
+					<div className='members-area-major-box-container'>
+						<div className='members-area-header-outer-container'>
+							<div className='members-area-header-container'>
+								CIBS Connect
+							</div>
+							<div className='members-area-button-outer-container'>
+								<div className='members-area-button-box' onClick={() => selectionFunc(2)}>
+									Access here
+								</div>
+							</div>
+						</div>
+						<div className='members-area-minor-text-container'>
+							CIBS Connect is an initiative created in order to provide more personal insights into the world of investment banking. We discuss pathways into the industry, skills and abilities needed for the role, and career progression. Tune in to our first two episodes with MDs from RBC Capital Markets and Morgan Stanley.
+						</div>
+					</div>
+
+					<div className='members-area-header-outer-container2'>
+						<div className='members-area-header-container'>
+							Social Media
+						</div>
+					</div>
+					<div className='members-area-minor-text-container'>
+						Follow CIBS on all social media to keep up-to-date with our events and competitions. Our LinkedIn page is also updated with links to the latest articles from our research group, which are a valuable resource in keeping in touch with the latest industry developments. Every week, four commercial awareness quiz questions are posted to our Instagram - a great way to see how much you've retained from this week's news! Finally, on Facebook, you can find all upcoming workshops and socials.
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+const MembersResources = () => {
+	return(
+		<>
+			<div className='members-banner-container'>
+				<div className='members-outer2'>
+					<div className='members-header-container'>
+						<p>RESOURCES</p>
+					</div>
+					<div className='thinLine'></div>
+					<div className='members-minor-container'>
+						<p>
+							We are currently in the process of preparing a series of guides. We intend to produce a comprehensive set of guides over the next two years and we will alert you as we produce them through our Roundups and social media platforms. You can find the guides that we have created so far linked below. Please do not share these materials with others, but feel free to encourage your friends to join CIBS!
+						</p>
+					</div>
+
+					<div className='resources-list-outer'>
+						<div className='resources-list-header'>Application Guides</div>
+
+						<div className='resources-box-container'>
+							<div className='image-events-outer-container'>
+								<Image
+									src={springGuide_img}
+									style={{objectFit: 'cover'}}
+									fill={true}
+									alt="initiatives pic"
+								/>
+							</div>
+
+							<div className='events-box-text-outer-container'>
+								<div className='events-box-text-header-container'>
+									Spring Week Guide
+								</div>
+								<div className='events-box-text-body-container'>
+									Our first application guide provides a wealth of tips and advice across every stage of the application process. The unique firm-specific portion of our guide covers application processes by firm, and even includes unique cultural aspects of each firm. Each firm-specific page of our guide was compiled through consultation with a past spring or summer intern at that firm.
+								</div>
+								<div className='events-box-text-button-outer-container'>
+									<div className='events-box-text-button-container'>
+										Access
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className='resources-list-header'>M&A Technicals Workshops</div>
+
+						<div className='resources-box-container'>
+							<div className='image-events-outer-container'>
+								<Image
+									src={workshop1_img}
+									style={{objectFit: 'cover'}}
+									fill={true}
+									alt="initiatives pic"
+								/>
+							</div>
+
+							<div className='events-box-text-outer-container'>
+								<div className='events-box-text-header-container'>
+									M&A Technicals #1: Accounting Fundamentals
+								</div>
+								<div className='events-box-text-body-container'>
+									Grab some drinks and get to know members of the committee! This should be a great opportunity to find out more about what it's like being on the CIBS committee. We'll be ready to answer questions regarding the roles, investment banking and anything else you want to ask!
+								</div>
+								<div className='events-box-text-button-outer-container'>
+									<div className='events-box-text-button-container'>
+										Watch now
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className='resources-box-container'>
+							<div className='image-events-outer-container'>
+								<Image
+									src={workshop2_img}
+									style={{objectFit: 'cover'}}
+									fill={true}
+									alt="initiatives pic"
+								/>
+							</div>
+
+							<div className='events-box-text-outer-container'>
+								<div className='events-box-text-header-container'>
+									M&A Technicals #2: Valuation Methods
+								</div>
+								<div className='events-box-text-body-container'>
+									Grab some drinks and get to know members of the committee! This should be a great opportunity to find out more about what it's like being on the CIBS committee. We'll be ready to answer questions regarding the roles, investment banking and anything else you want to ask!
+								</div>
+								<div className='events-box-text-button-outer-container'>
+									<div className='events-box-text-button-container'>
+										Watch now
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className='resources-box-container'>
+							<div className='image-events-outer-container'>
+								<Image
+									src={workshop3_img}
+									style={{objectFit: 'cover'}}
+									fill={true}
+									alt="initiatives pic"
+								/>
+							</div>
+
+							<div className='events-box-text-outer-container'>
+								<div className='events-box-text-header-container'>
+									M&A Technicals #3: Intrinsic Valuation - DCF
+								</div>
+								<div className='events-box-text-body-container'>
+									Grab some drinks and get to know members of the committee! This should be a great opportunity to find out more about what it's like being on the CIBS committee. We'll be ready to answer questions regarding the roles, investment banking and anything else you want to ask!
+								</div>
+								<div className='events-box-text-button-outer-container'>
+									<div className='events-box-text-button-container'>
+										Watch now
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</>
+	)
+}
+
+const MembersCibsConnect = () => {
+	return(
+		<>
+			<div className='members-banner-container'>
+				<div className='members-outer2'>
+					<div className='members-header-container'>
+						<p>CIBS CONNECT</p>
+					</div>
+					<div className='thinLine'></div>
+					<div className='members-minor-container'>
+						<p>
+							CIBS Connect is a brand-new interview series which will give all students a deeper understanding of the world of finance straight from industry experts. Episodes will cover a wide range of topics, including application tips, insights into diversity & inclusion, experiences within finance, and many more.
+						</p>
+					</div>
+
+					<div className='resources-list-outer'>
+						<div className='resources-list-header'>CIBS Connect Episodes</div>
+
+						<div className='resources-box-container'>
+							<div className='image-events-outer-container'>
+								<Image
+									src={connect_img}
+									style={{objectFit: 'cover'}}
+									fill={true}
+									alt="initiatives pic"
+								/>
+							</div>
+
+							<div className='events-box-text-outer-container'>
+								<div className='events-box-text-header-container'>
+									#1 Matthew Coakes, RBC MD
+								</div>
+								<div className='events-box-text-body-container'>
+									Our first interview is with Matthew Coakes, Managing Director at Royal Bank of Canada (RBC) Capital Markets. Matthew studied Engineering at the University of Cambridge, and discusses why he entered the investment banking industry, managing stress on the job, and his career trajectory. He also discusses the focus on both renewables and non-renewables at RBC, as well as supporting businesses to make the transition to more sustainable energy sources.
+								</div>
+								<div className='events-box-text-button-outer-container'>
+									<div className='events-box-text-button-container'>
+										Watch now
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className='resources-box-container'>
+							<div className='image-events-outer-container'>
+								<Image
+									src={connect_img}
+									style={{objectFit: 'cover'}}
+									fill={true}
+									alt="initiatives pic"
+								/>
+							</div>
+
+							<div className='events-box-text-outer-container'>
+								<div className='events-box-text-header-container'>
+									#2 Christopher Lipscomb, Morgan Stanley MD
+								</div>
+								<div className='events-box-text-body-container'>
+									Next, we chat with Christopher Lipscomb, Managing Director at Morgan Stanley. Christopher studied Philosophy at the University of Cambridge, before pivoting to the investment banking industry. In this episode of CIBS Connect, he discusses how skills gained through his degree were applicable to IB, what the atmosphere is like at work, and how he spends his spare time.
+								</div>
+								<div className='events-box-text-button-outer-container'>
+									<div className='events-box-text-button-container'>
+										Watch now
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default function MembersArea() {
+
+	const [currentSelection, setCurrentSelection] = useState(0);
+
+	const selectionChange = (id) => {
+        setCurrentSelection(id);
+    }
+
+	return (
+		<>
+			<div className='members-navbar'>
+				<div className='members-navbar-inner'>
+					<div className='members-navbarlinks-outer-container'>
+						<div className='members-navbar-link' onClick={() => selectionChange(0)}>Overview</div>
+						<div className='members-navbar-link' onClick={() => selectionChange(1)}>Resources</div>
+						<div className='members-navbar-link' onClick={() => selectionChange(2)}>CIBS Connect</div>
+					</div>
+				</div>
+			</div>
+			{currentSelection===0 && <MembersOverview selectionFunc={selectionChange}/>}
+			{currentSelection===1 && <MembersResources />}
+			{currentSelection===2 && <MembersCibsConnect />}
+		</>
+	)
+}
