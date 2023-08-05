4c0a289309f74cbe8c7eab6603ae721df34fdad5
oskari83 <oskpelto@gmail.com>
2023-08-05 12:39:46 +0300

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Optional: Add a trailing slash to all paths `/about` -> `/about/`
-	trailingSlash: true,
-	//Optional: Change the output directory `out` -> `dist`
-	distDir: 'dist',
-	crossOrigin: 'anonymous',
 	images: { unoptimized: true },
 }  
 
